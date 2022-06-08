da97425cefd105eb5cb6b67df72128a6cc98c1cd
Richard Anchieta <richard.anchieta@stemativa.com.br>
2022-06-08 01:48:49 -0300

    wip: solução inicial

---
+++ components/LayoutWrapper.tsx
@@ -0,0 +1,62 @@
+import siteMetadata from '@/data/siteMetadata'
+import headerNavLinks from '@/data/headerNavLinks'
+import Logo from '@/data/logo.svg'
+import Link from './Link'
+import SectionContainer from './SectionContainer'
+import Footer from './Footer'
+import MobileNav from './MobileNav'
+import ThemeSwitch from './ThemeSwitch'
+import { ReactNode } from 'react'
+
+interface Props {
+  children: ReactNode
+}
+
+const LayoutWrapper = ({ children }: Props) => {
+  return (
+    <SectionContainer>
+      <div className="fixed left-0 z-40 h-[60px] w-full bg-gray-100 dark:bg-gray-800"></div>
+      <header className="l-0 fixed left-0 top-0 z-50 mx-0 flex w-full items-center justify-between px-2 py-2">
+        <div className="container mx-auto flex items-center justify-between  max-w-3xl sm:px-6 xl:max-w-5xl xl:px-0">
+          <div>
+            <Link href="/" aria-label={siteMetadata.headerTitle}>
+              <div className="flex items-center justify-between">
+                <div className="mr-3 w-[42px]">
+                  <Logo />
+                </div>
+                {typeof siteMetadata.headerTitle === 'string' ? (
+                  <div className="hidden h-6 text-2xl font-semibold sm:block">
+                    {siteMetadata.headerTitle}
+                  </div>
+                ) : (
+                  siteMetadata.headerTitle
+                )}
+              </div>
+            </Link>
+          </div>
+          <div className="flex items-center text-base leading-5">
+            <div className="hidden sm:block">
+              {headerNavLinks.map((link) => (
+                <Link
+                  key={link.title}
+                  href={link.href}
+                  className="p-1 font-medium text-gray-900 dark:text-gray-100 sm:p-4"
+                >
+                  {link.title}
+                </Link>
+              ))}
+            </div>
+            <ThemeSwitch />
+            <MobileNav />
+          </div>
+        </div>
+      </header>
+      <div className="flex h-screen flex-col justify-between">
+        <main className="mb-auto mt-12">{children}</main>
+        <Footer />
+      </div>
+    </SectionContainer>
+  )
+}
+
+export default LayoutWrapper
