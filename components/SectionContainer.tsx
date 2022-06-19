a667fd603482ea7c3e713d9d8ed32e0594f43a0e
Richard Anchieta <richard.anchieta@stemativa.com.br>
2022-06-19 17:21:56 -0300

    fix: ajusta textos empt-br e layout padrão

--- components/SectionContainer.tsx
+++ components/SectionContainer.tsx
@@ -6,6 +6,11 @@ interface Props {
 
 export default function SectionContainer({ children }: Props) {
   return (
-    <div className="mx-auto h-auto max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0">{children}</div>
+    <div
+      style={{ border: 'solid 1px transparent' }}
+      className="mx-auto h-auto max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0"
+    >
+      {children}
+    </div>
   )
 }
