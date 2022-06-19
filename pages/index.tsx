98c633af2afc6a088f2dd8c6b74699ae5793acb4
Richard Anchieta <richard.anchieta@stemativa.com.br>
2022-06-19 17:44:42 -0300

    fix: oculta cadastro de newsletter temporariamente

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,22 +21,22 @@ export default function Home({ posts }: InferGetStaticPropsType<typeof getStatic
     <>
       <PageSEO title={siteMetadata.title} description={siteMetadata.description} />
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
-        <div className="grid gap-4 md:grid-cols-2">
-          <div className="col-6 space-y-2 pt-6 pb-8 md:space-y-5">
-            <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-              Artigos recentes
-            </h1>
-            <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-              {siteMetadata.description}
-            </p>
-          </div>
+        {/* <div className="grid gap-4 md:grid-cols-2"> */}
+        <div className="col-6 space-y-2 pt-6 pb-8 md:space-y-5">
+          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+            Artigos recentes
+          </h1>
+          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
+            {siteMetadata.description}
+          </p>
+        </div>
 
-          {siteMetadata.newsletter.provider !== '' && (
+        {/* {siteMetadata.newsletter.provider !== '' && (
             <div className="flex items-center justify-center pb-8">
               <NewsletterForm />
             </div>
-          )}
-        </div>
+          )} */}
+        {/* </div> */}
 
         <ul className="divide-y divide-gray-200 dark:divide-gray-700">
           {!posts.length && 'Nenhum post encontrado.'}
